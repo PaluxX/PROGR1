85f4848d36d5891d477396263c06fafaf22fb7ca
PaluxX <agus.palombo01@hotmail.com>
2024-10-25 07:21:00 -0300

    Creación nivel

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMov : MonoBehaviour
 {
     [SerializeField] private float _movSpeed = 3f;
@@ -9,8 +10,16 @@ public class PlayerMov : MonoBehaviour
     [SerializeField] private float _rotSpeed = 120f;
 
 
-    private float _xAxis = 0, _zAxis = 0;
+    private float _xAxis = 0f, _zAxis = 0f;
     private Vector3 _dir = new();
+
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb= GetComponent<Rigidbody>();
+        _rb.constraints= RigidbodyConstraints.FreezeRotation;
+    }
 
     private void Update()
     {
